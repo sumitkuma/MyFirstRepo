eda93d07575973b66db7ee2bdb3cf48664b87c06
sumitkuma <sumit_k@hcl.com>
2015-06-10 18:03:07 +0530

    added files

---
+++ codeFirstSample/codeFirstSample/codeFirstSample/Models/BooksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace codeFirstSample.Models
+{
+    public class BooksDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ codeFirstSample/codeFirstSample/codeFirstSample/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace codeFirstSample.Models
+{
+    [Table("Reviews")] // Table name
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [ForeignKey("Book")]
+        public int BookID { get; set; }
+        public string ReviewText { get; set; }
+
+        // This will keep track of the book this review belong too
+        public virtual Book Book { get; set; }
+    }
+}
